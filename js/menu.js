58db986336b4894a22e2b35c86c44bae429653c6
JakobJR <jakobjr@hotmail.com>
2021-08-29 15:31:24 +0200

    Move to live
    still testing mobile device

--- js/menu.js
+++ js/menu.js
@@ -6,6 +6,6 @@ document.querySelector('#mobile-menu-btn').addEventListener('click', () => {
     document.querySelector('#mobile-menu-icon').classList.toggle('spin');
 });
 
-document.querySelectorAll('.icon-wrapper').forEach(elem => elem.addEventListener("click", function() {
+document.querySelectorAll('.icon-wrapper').forEach(elem => elem.addEventListener("click", function(event) {
     event.target.nextElementSibling.classList.toggle('show-info');
 }));
